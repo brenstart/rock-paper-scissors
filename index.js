b32233755c31bcce95def10b2aa385e97141dc5b
brenstart <148547900+brenstart@users.noreply.github.com>
2023-11-29 21:04:31 +0800

    Add function that starts the battle.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+// This function returns result of Computer based on random num from 1 to 3
 function getComputerChoice () {
   let compResult =""
   let compRandomNum = Math.floor(Math.random()*3)+1
   if (compRandomNum === 1) {
     compResult = "Rock"
-    console.log(compRandomNum)
+   // console.log(compRandomNum)
   } else if (compRandomNum === 2) {
     compResult = "Paper"
-    console.log(compRandomNum)
+   // console.log(compRandomNum)
   } else {
     compResult = "Scissors"
-    console.log(compRandomNum)
+   // console.log(compRandomNum)
   }
 
   return compResult
 }
 console.log(getComputerChoice())
+
+
+// player and computer battles here
+function playerVsComp (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "It's a TIE! Battle again!"
+    } 
+}
+
+console.log(playerVsComp("rock", "rock"))

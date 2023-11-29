40bb43e29e415a2d591cd53f5b23c32c085057e3
brenstart <148547900+brenstart@users.noreply.github.com>
2023-11-29 20:36:25 +0800

    Check if computer result is equal to number
    Using console.log and returning it to check if the computer result of rock, paper or scissors is equal to the number specified.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function getComputerChoice () {
+  let compResult =""
   let compRandomNum = Math.floor(Math.random()*3)+1
-  console.log(compRandomNum)
-}
+  if (compRandomNum === 1) {
+    compResult = "Rock"
+    console.log(compRandomNum)
+  } else if (compRandomNum === 2) {
+    compResult = "Paper"
+    console.log(compRandomNum)
+  } else {
+    compResult = "Scissors"
+    console.log(compRandomNum)
+  }
 
-getComputerChoice()
+  return compResult
+}
+console.log(getComputerChoice())

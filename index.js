4eff109e63497e5d8d5993126bd839d706c71b59
brenstart <148547900+brenstart@users.noreply.github.com>
2023-11-30 10:40:47 +0800

    Add scoreboard and loop the game to best of 5

--- index.js
+++ index.js
@@ -19,36 +19,67 @@ function getComputerChoice() {
 
 
 // player and computer battles here
+let compScore = 0
+let playerScore = 0
 let winMsg = "You Win!"
 let loseMsg = "You Lose!"
 function playerVsComp (playerSelection, computerSelection) {
 
-  playerSelection = playerSelection.toLowerCase()
+  playerSelection = playerSelection.toLowerCase() //should I make the input case insensitive during the prompt inputSelection function or here?
 
     if (playerSelection === computerSelection) {
-      console.log("Computer chose " + computerSelection)
+      console.log("Computer chose " + computerSelection + ". You chose " + playerSelection)
+      console.log(`Current score: You: ${playerScore} Computer:${compScore}`)
       return "It's a TIE! Battle again!";
       //playerVsComp(); //if tie, battle again
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-      console.log("Computer chose " + computerSelection)
+      console.log("Computer chose " + computerSelection + ". You chose " + playerSelection)
+      compScore += 1
+      console.log(`Current score: You: ${playerScore} Computer:${compScore}`)
       return loseMsg
+      
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      console.log("Computer chose " + computerSelection)
+      console.log("Computer chose " + computerSelection + ". You chose " + playerSelection)
+      playerScore += 1
+      console.log(`Current score: You: ${playerScore} Computer:${compScore}`)
       return winMsg
+      
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-      console.log("Computer chose " + computerSelection)
+      console.log("Computer chose " + computerSelection + ". You chose " + playerSelection)
+      compScore += 1
+      console.log(`Current score: You: ${playerScore} Computer:${compScore}`)
       return loseMsg
+      
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-      console.log("Computer chose " + computerSelection)
+      console.log("Computer chose " + computerSelection + ". You chose " + playerSelection)
+      playerScore += 1
+      console.log(`Current score: You: ${playerScore} Computer:${compScore}`)
       return winMsg
+      
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-      console.log("Computer chose " + computerSelection)
+      console.log("Computer chose " + computerSelection + ". You chose " + playerSelection)
+      compScore += 1
+      console.log(`Current score: You: ${playerScore} Computer:${compScore}`)
       return loseMsg
+      
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-      console.log("Computer chose " + computerSelection)
+      console.log("Computer chose " + computerSelection + ". You chose " + playerSelection)
+      playerScore += 1
+      console.log(`Current score: You: ${playerScore} Computer:${compScore}`)
       return winMsg
+      
     }
   }
+
+
+
+
+
+
+//game function
+
+function game() {
+for (let i = 1; i < 6; i++){
 
 
 // This requests for an input from the player
@@ -63,3 +94,9 @@ let compSelection = getComputerChoice
 
 //This inititates the battle sequence
 console.log(playerVsComp(inputSelection(), getComputerChoice())) //made the func inputSelection add it's result here as an argument
+
+}
+
+}
+
+game()

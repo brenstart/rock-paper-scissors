13c8cb66ea42f8492eefaf8ff479a32756ba8099
brenstart <148547900+brenstart@users.noreply.github.com>
2023-11-30 09:54:00 +0800

    Add player and comp's choice as arguments
    Finally figured out how to pass functions as arguments to be used by the playerVsComp function.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // This function returns result of Computer based on random num from 1 to 3
-function getComputerChoice () {
+function getComputerChoice() {
   let compResult =""
   let compRandomNum = Math.floor(Math.random()*3)+1
   if (compRandomNum === 1) {
-    compResult = "Rock"
+    compResult = "rock"
    // console.log(compRandomNum)
   } else if (compRandomNum === 2) {
-    compResult = "Paper"
+    compResult = "paper"
    // console.log(compRandomNum)
   } else {
-    compResult = "Scissors"
+    compResult = "scissors"
    // console.log(compRandomNum)
   }
 
   return compResult
 }
-//console.log(getComputerChoice())
+// console.log("computer choice: " + getComputerChoice())
 
 
 // player and computer battles here
@@ -26,32 +26,40 @@ function playerVsComp (playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase()
 
     if (playerSelection === computerSelection) {
+      console.log("Computer chose " + computerSelection)
       return "It's a TIE! Battle again!";
       //playerVsComp(); //if tie, battle again
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+      console.log("Computer chose " + computerSelection)
       return loseMsg
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      console.log("Computer chose " + computerSelection)
       return winMsg
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      console.log("Computer chose " + computerSelection)
       return loseMsg
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+      console.log("Computer chose " + computerSelection)
       return winMsg
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      console.log("Computer chose " + computerSelection)
       return loseMsg
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      console.log("Computer chose " + computerSelection)
       return winMsg
     }
   }
 
 
-
+// This requests for an input from the player
 let selection =""
 function inputSelection() {
     selection = prompt("Rock, Paper or Scissors?")
+    return selection
 }
-    inputSelection()
+   // inputSelection()
 
-
+let compSelection = getComputerChoice
 
 //This inititates the battle sequence
-console.log(playerVsComp(`${selection}`, `rock`)) //made the func inputSelection add it's result here as an argument
+console.log(playerVsComp(inputSelection(), getComputerChoice())) //made the func inputSelection add it's result here as an argument
